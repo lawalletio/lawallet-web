60ef65482f0e39e6e8b00deb369211b58221bb2f
ferpa <feririarte.1996@gmail.com>
2024-07-30 14:42:13 -0300

    rename transaction cache variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const STORAGE_IDENTITY_KEY = 'identity';
-export const CACHE_TXS_KEY = 'cache_txs';
+export const CACHE_TXS_KEY = 'cached_txs';
 export const CACHE_BACKUP_KEY = 'backup';
 
 export const APP_NAME = 'LaWallet';
