9acaa3df825c4d29f0bc7cb7844e59218a6f39e4
Carlos Dugarte <soycarlosdugarte@gmail.com>
2024-08-25 08:37:04 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,53 +1,15 @@
-// /** @type {import('next').NextConfig} */
-
-// import createNextIntlPlugin from 'next-intl/plugin';
-// import withSerwistInit from '@serwist/next';
-
-// import { createRequire } from 'module';
-// const require = createRequire(import.meta.url);
-
-// const withNextIntl = createNextIntlPlugin();
-// const { version } = require('./package.json');
-
-// const withSerwist = withSerwistInit({
-//   swSrc: 'src/utils/sw.ts',
-//   swDest: 'public/sw.js',
-//   disable: process.env.NODE_ENV === 'development',
-// });
-
-// const nextConfig = {
-//   reactStrictMode: false,
-//   trailingSlash: true,
-//   swcMinify: true,
-//   transpilePackages: ['@lawallet/react', '@lawallet/utils', '@lawallet/ui'],
-//   compiler: {
-//     styledComponents: true,
-//   },
-//   env: {
-//     version,
-//   },
-// };
-
-// export default withNextIntl(withSerwist(nextConfig));
-
-//New code 
-
 /** @type {import('next').NextConfig} */
 
-// const withPWA = require('@ducanh2912/next-pwa').default({
-//   dest: 'public',
-//   skipWaiting: true,
-//   register: true
-// })
+import createNextIntlPlugin from 'next-intl/plugin';
+import withSerwistInit from '@serwist/next';
 
-const createNextIntlPlugin = require('next-intl/plugin');
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const withNextIntl = createNextIntlPlugin();
-
 const { version } = require('./package.json');
 
-const withSerwist = require('@serwist/next').default({
-  // Note: This is only an example. If you use Pages Router,
-  // use something else that works, such as "service-worker/index.ts".
+const withSerwist = withSerwistInit({
   swSrc: 'src/utils/sw.ts',
   swDest: 'public/sw.js',
   disable: process.env.NODE_ENV === 'development',
@@ -66,4 +28,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNextIntl(withSerwist(nextConfig));
+export default withNextIntl(withSerwist(nextConfig));
