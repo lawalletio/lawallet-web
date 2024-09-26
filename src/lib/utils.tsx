4a560eb41dca66aeb9f56de03d8a9edf01145a89
Jona <llamasjonat@gmail.com>
2024-09-25 21:11:05 -0300

    fix(lib): integrate functions for domains on Utils

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -4,3 +4,45 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const extractDomain = (url: string): string => {
+  try {
+    // Crear un objeto URL
+    const parsedUrl = new URL(url);
+
+    // Extraer el hostname (dominio completo)
+    const hostname = parsedUrl.hostname;
+
+    // Remover el prefijo 'www.' si está presente
+    const domain = hostname.startsWith('www.') ? hostname.slice(4) : hostname;
+
+    return domain;
+  } catch (error) {
+    return url;
+  }
+};
+
+export const normalizeUrl = (input: string): string => {
+  try {
+    let domain: string;
+
+    // Detectar si el input ya es una URL completa
+    if (input.startsWith('http://') || input.startsWith('https://')) {
+      const parsedUrl = new URL(input);
+      domain = parsedUrl.hostname;
+    } else {
+      domain = input;
+    }
+
+    // Eliminar el prefijo 'www.' si está presente
+    domain = domain.startsWith('www.') ? domain.slice(4) : domain;
+
+    // Construir la URL completa
+    const url = `https://${domain}`;
+
+    return url;
+  } catch (error) {
+    console.error('Error al procesar el input:', error);
+    return input;
+  }
+};
