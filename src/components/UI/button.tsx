c467701e1576862e2de0db4392f5d4a211b49686
Jona <llamasjonat@gmail.com>
2025-04-05 20:08:17 -0300

    style(component): changed base styles for Button and Input

--- src/components/UI/button.tsx
+++ src/components/UI/button.tsx
@@ -5,19 +5,19 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-full font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-full font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-card text-primary shadow-sm hover:bg-card/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        destructive: 'bg-card text-destructive shadow-sm hover:bg-card',
+        outline: 'text-primary border border-card bg-background shadow-sm hover:bg-primary/20 hover:text-primary',
+        secondary: 'bg-card text-primary shadow-sm hover:bg-primary/20',
+        ghost: 'text-primary hover:bg-primary/20 hover:text-primary',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 text-md',
+        default: 'h-10 px-4 py-2 text-md',
         sm: 'h-8 px-3 text-sm',
         lg: 'h-10 px-8',
         icon: 'h-8 w-8',
