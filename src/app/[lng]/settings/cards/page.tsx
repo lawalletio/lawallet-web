26e4a18dc1aa68c54a3b9bfc063519b207d94373
Jona <llamasjonat@gmail.com>
2025-04-08 22:32:35 -0300

    fix(page): integrate Toast on Cards

--- src/app/[lng]/settings/cards/page.tsx
+++ src/app/[lng]/settings/cards/page.tsx
@@ -5,7 +5,7 @@ import { Design } from '@lawallet/react/types';
 import { Container, Divider, Flex, Loader } from '@lawallet/ui';
 
 import { useCardsContext } from '@/context/CardsContext';
-import { useNotifications } from '@/context/NotificationsContext';
+import { useToast } from '@/hooks/use-toast';
 
 import Navbar from '@/components/Layout/Navbar';
 import AddNewCardModal from './components/AddCard';
@@ -13,18 +13,18 @@ import DebitCard from './components/DebitCard';
 import EmptyCards from './components/EmptyCards';
 
 export default function Page() {
-  const notifications = useNotifications();
   const { cardsData, cardsConfig, loadInfo, toggleCardStatus } = useCardsContext();
 
   const t = useTranslations();
+  const { toast } = useToast();
 
   const handleToggleStatus = async (uuid: string) => {
     const toggled: boolean = await toggleCardStatus(uuid);
     if (toggled)
-      notifications.showAlert({
-        title: '',
+      toast({
         description: t('TOGGLE_STATUS_CARD_SUCCESS'),
-        type: 'success',
+        variant: 'default',
+        duration: 1400,
       });
 
     return toggled;
